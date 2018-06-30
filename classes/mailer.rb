14c310bd74f8c4bca2b8419f4f25ae08fa061962
Marcin K <marcin.knychala@bitgamelabs.com>
2018-06-30 18:23:07 +0200

    Create draft mailer

---
+++ classes/mailer.rb
@@ -0,0 +1,11 @@
+require 'dotenv/load'
+require 'net/smtp'
+
+def send_mail
+  message = 'This is the body of a test message'
+  smtp = Net::SMTP.new 'smtp.gmail.com', 587
+  smtp.enable_starttls
+  smtp.start('gmail.com', ENV['EMAIL_ADDRESS'], ENV['EMAIL_PASSWORD'], :login) do |smtp|
+    smtp.send_message message, ENV['EMAIL_ADDRESS'], '***REMOVED***'
+  end
+end
