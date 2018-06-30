9160ca7eae161217b45baf52bf5fc66aff42fe1d
Marcin K <marcin.knychala@bitgamelabs.com>
2018-06-30 16:32:41 +0200

    Scrap live events - declare the method earlier in the code
    Otherwise it's not accessible

--- scrap_live_events.rb
+++ scrap_live_events.rb
@@ -22,13 +22,13 @@ end
 puts 'Starting event scraper, hit CTRL+D or Q and then ENTER to quit'
 web_scraper = WebScraper.new
 
+def create_exit_on_input_thread
+  Thread.new { puts 'Goodbye!' && exit if quit? }
+end
+
 loop do
   web_scraper.run
   exit_on_input_thread = create_exit_on_input_thread
   sleep Settings.time_interval
   exit_on_input_thread.terminate
-end
-
-def create_exit_on_input_thread
-  Thread.new { puts 'Goodbye!' && exit if quit? }
 end
