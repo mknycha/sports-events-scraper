7b40052df506b0e94dc74db2a7ceaf1856bdedf7
Marcin K <marcin.knychala@bitgamelabs.com>
2018-06-30 16:14:55 +0200

    Scrap live events - remove TODO

--- scrap_live_events.rb
+++ scrap_live_events.rb
@@ -4,10 +4,6 @@ require_relative 'settings'
 require_relative 'classes/event'
 require_relative 'classes/web_scraper'
 
-# TODO:
-# - Escape and return message when the element with tables was not found on page
-# - Quit app with user input. User input can be received anytime, but the iteration should be finished anyway
-
 def quit?
   # See if a 'Q' has been typed yet
   while c = STDIN.read_nonblock(1)
