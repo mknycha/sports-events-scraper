3834aa477bd841695d263b90185a436e6cc602f9
Marcin K <marcin.knychala@bitgamelabs.com>
2018-06-30 22:52:52 +0200

    Mailer class - refactor

--- classes/mailer.rb
+++ classes/mailer.rb
@@ -12,10 +12,15 @@ class Mailer
 
   def send_mail
     message = 'This is the body of a test message'
-    smtp = Net::SMTP.new 'smtp.gmail.com', 587
-    smtp.enable_starttls
-    smtp.start('gmail.com', ENV['EMAIL_ADDRESS'], ENV['EMAIL_PASSWORD'], :login) do |smtp|
-      smtp.send_message message, ENV['EMAIL_ADDRESS'], Settings.recipient_email
-    end
+    smtp = create_smtp_with_tls_enabled
+    smtp.start('gmail.com', ENV['EMAIL_ADDRESS'], ENV['EMAIL_PASSWORD'], :login)
+    smtp.send_message message, ENV['EMAIL_ADDRESS'], Settings.recipient_email
+    smtp.finish
+  end
+
+  private
+
+  def create_smtp_with_tls_enabled
+    Net::SMTP.new('smtp.gmail.com', 587).enable_starttls
   end
 end
