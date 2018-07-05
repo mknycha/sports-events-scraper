b8105f995f20513f82660d817251de9a48eede62
Marcin K <marcin.knychala@bitgamelabs.com>
2018-07-05 22:10:10 +0200

    Mailer - make emails sent with mail gem

--- classes/mailer.rb
+++ classes/mailer.rb
@@ -2,6 +2,8 @@
 
 require 'dotenv/load'
 require 'net/smtp'
+require 'mail'
+require_relative '../settings'
 require_relative 'events_html_table'
 
 class Mailer
@@ -14,16 +16,34 @@ class Mailer
   end
 
   def send_mail
-    message = 'This is the body of a test message'
-    smtp = create_smtp_with_tls_enabled
-    smtp.start('gmail.com', ENV['EMAIL_ADDRESS'], ENV['EMAIL_PASSWORD'], :login)
-    smtp.send_message message, ENV['EMAIL_ADDRESS'], Settings.recipient_email
-    smtp.finish
+    Mail.defaults do
+      delivery_method :smtp,
+                      address: 'smtp.gmail.com',
+                      port: 587,
+                      user_name: ENV['EMAIL_ADDRESS'],
+                      password: ENV['EMAIL_PASSWORD'],
+                      authentication: :plain,
+                      enable_starttls_auto: true
+    end
+    message.deliver!
   end
 
   private
 
   def create_smtp_with_tls_enabled
-    Net::SMTP.new('smtp.gmail.com', 587).enable_starttls
+    smtp = Net::SMTP.new('smtp.gmail.com', 587)
+    smtp.enable_starttls
+    smtp
+  end
+
+  def message
+    message_body = '<h3>Hello! These events might interest you</h3>' + @events_html_table.to_s
+    Mail.new do
+      to      ::Settings.recipient_email
+      from    ENV['EMAIL_ADDRESS']
+      subject 'Live events report'
+      content_type 'text/html; charset=UTF-8'
+      body message_body
+    end
   end
 end
