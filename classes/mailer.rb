e6995342ffe5832b94d881e8e8695927f9efa38e
Marcin K <marcin.knychala@bitgamelabs.com>
2018-07-12 21:28:21 +0200

    Mailer: turn instance methods to class methods

--- classes/mailer.rb
+++ classes/mailer.rb
@@ -13,27 +13,29 @@ class Mailer
     end
   end
 
-  def send_table_by_email(events_html_table_as_string)
+  def self.send_table_by_email(events_html_table_as_string)
     message(events_html_table_as_string).deliver!
   end
 
-  private
+  class << self
+    private
 
-  def create_smtp_with_tls_enabled
-    smtp = Net::SMTP.new('smtp.gmail.com', 587)
-    smtp.enable_starttls
-    smtp
-  end
+    def create_smtp_with_tls_enabled
+      smtp = Net::SMTP.new('smtp.gmail.com', 587)
+      smtp.enable_starttls
+      smtp
+    end
 
-  def message(events_html_table_as_string)
-    message_body = '<h3>Hello! These events might interest you</h3>' +
-                   events_html_table_as_string
-    Mail.new do
-      to      ::Settings.recipient_email
-      from    ENV['EMAIL_ADDRESS']
-      subject 'Live events report'
-      content_type 'text/html; charset=UTF-8'
-      body message_body
+    def message(events_html_table_as_string)
+      message_body = '<h3>Hello! These events might interest you</h3>' +
+                     events_html_table_as_string
+      Mail.new do
+        to      ::Settings.recipient_email
+        from    ENV['EMAIL_ADDRESS']
+        subject 'Live events report'
+        content_type 'text/html; charset=UTF-8'
+        body message_body
+      end
     end
   end
 end
