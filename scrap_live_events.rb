935b93f678e1383ac5c07b6fbe1485ea9dbda426
Marcin K <marcin.knychala@bitgamelabs.com>
2018-07-13 21:07:55 +0200

    Remove code for reading keyboard input

--- scrap_live_events.rb
+++ scrap_live_events.rb
@@ -8,32 +8,8 @@ require_relative 'classes/web_scraper'
 require_relative 'classes/events_html_table'
 require_relative 'classes/mailer'
 
-def quit?
-  # See if a 'Q' has been typed yet
-  while c = STDIN.read_nonblock(1)
-    return true if %w[Q q].include?(c)
-  end
-  false # No 'Q' found
-rescue Errno::EINTR, Errno::EAGAIN
-  # when the device is slow or nothing to be read
-  false
-rescue EOFError
-  # quit on the end of the input stream
-  # (user hit CTRL-D)
-  true
-end
-
 puts 'Starting event scraper, hit CTRL+D or Q and then ENTER to quit'
 web_scraper = WebScraper.new
-
-def create_exit_on_input_thread
-  Thread.new do
-    if quit?
-      puts 'Goodbye!'
-      exit
-    end
-  end
-end
 
 trap 'SIGINT' do
   puts 'Goodbye!'
@@ -42,7 +18,5 @@ end
 
 loop do
   web_scraper.run
-  exit_on_input_thread = create_exit_on_input_thread
   sleep Settings.time_interval
-  exit_on_input_thread.terminate
 end
