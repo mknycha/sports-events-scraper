21e267a82b97dc0045fdb301daf5452e54d451c8
Marcin K <marcin.knychala@bitgamelabs.com>
2018-07-13 20:53:34 +0200

    Add specs for mailer class

---
+++ spec/mailer_spec.rb
@@ -0,0 +1,58 @@
+require 'mail'
+require 'dotenv/load'
+require_relative '../settings'
+require_relative '../classes/event'
+require_relative '../classes/events_html_table'
+require_relative '../classes/mailer'
+
+Mail.defaults do
+  delivery_method :test
+end
+
+describe Mailer do
+  include Mail::Matchers
+
+  before do
+    Mail::TestMailer.deliveries.clear
+    allow(Settings).to receive(:recipient_email).and_return(recipient_email)
+  end
+
+  let(:recipient_email) { 'fake.email@example.com' }
+
+  describe '#send_table_by_email' do
+    let(:event1) do
+      Event.new('Team A vs Team B', '1-3', '32:43', 'https://link.costam')
+    end
+    let(:event2) do
+      Event.new('Team C vs Team D', '7-0', '22:23', 'https://link.costam_innego')
+    end
+
+    context 'when gets a valid table as an input' do
+      let(:events_table) { EventsHtmlTable.new }
+      let(:action) { described_class.send_table_by_email(events_table.to_s) }
+      let(:expected_message_body) do
+        '<h3>Hello! These events might interest you</h3>' + events_table.to_s
+      end
+
+      before do
+        events_table.add_event(event1)
+        events_table.add_event(event2)
+      end
+
+      it 'sends an email including html table with events' do
+        action
+        expect(Mail::TestMailer.deliveries.length).to eq(1)
+      end
+
+      it 'sends an email to the proper recipient' do
+        action
+        is_expected.to have_sent_email.to(recipient_email)
+      end
+
+      it 'includes table with events info in the email body' do
+        action
+        is_expected.to have_sent_email.with_body(expected_message_body)
+      end
+    end
+  end
+end
