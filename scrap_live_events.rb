4889018f413ab9f3b7661c9edf53cc91329abb97
Marcin K <marcin.knychala@bitgamelabs.com>
2018-07-21 18:46:11 +0200

    ScrapLiveEvents: put code in a class

--- scrap_live_events.rb
+++ scrap_live_events.rb
@@ -8,19 +8,27 @@ require_relative 'classes/web_scraper'
 require_relative 'classes/events_html_table'
 require_relative 'classes/mailer'
 
-puts 'Starting event scraper, hit CTRL+C to quit'
-web_scraper = WebScraper.new
+class App
+  def run
+    puts 'Starting event scraper, hit CTRL+C to quit'
+    web_scraper = WebScraper.new
 
-trap 'SIGINT' do
-  puts 'Goodbye!'
-  exit 130
+    loop do
+      web_scraper.run
+      sleep_thread.join
+    end
+  end
+
+  private
+
+  trap 'SIGINT' do
+    puts 'Goodbye!'
+    exit 130
+  end
+
+  def sleep_thread
+    Thread.new { sleep Settings.time_interval }
+  end
 end
 
-def sleep_thread
-  Thread.new { sleep Settings.time_interval }
-end
-
-loop do
-  web_scraper.run
-  sleep_thread.join
-end
+App.new.run
