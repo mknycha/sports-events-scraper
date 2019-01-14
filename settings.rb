787b765d47ac07b956eff6a2a0fb6095c601a142
Marcin K <marcin.knychala@bitgamelabs.com>
2019-01-14 21:26:19 +0100

    Change the settings - time range for analysis, timeframe for running and email

--- settings.rb
+++ settings.rb
@@ -4,17 +4,17 @@ Settings = OpenStruct.new(
   reporting_conditions: {
     # If live event time is after this number of minutes,
     # it will be qualified for reporting
-    after_minutes: 45,
+    after_minutes: 53,
     # If live event time is after this number of minutes,
     # it will not be qualified for reporting
-    before_minutes: 60,
+    before_minutes: 57,
     # If the goal difference is equal to this number (absolute value),
     # it will be qualified for reporting
     goal_difference: 1
   },
   # Specifies how often scraper is to be run, in seconds.
   # If set to 60, Web Scraper will be checking website every 60 seconds
-  time_interval: 300,
+  time_interval: 120,
   # Info about events will be sent to this email
   recipient_email: '***REMOVED***'
 )
