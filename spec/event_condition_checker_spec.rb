57ef0533ae1e9eee577766d0a8510bdd7a43827e
Marcin K <marcin.knychala@bitgamelabs.com>
2019-08-21 23:22:01 +0200

    Report events only when their model score and ball possession is above cutoff values

--- spec/event_condition_checker_spec.rb
+++ spec/event_condition_checker_spec.rb
@@ -97,4 +97,53 @@ describe EventConditionChecker do
       )
     end
   end
+
+  describe '#should_be_reported?' do
+    let(:base_event) do
+      Event.new('A vs B', '54:01', '0-1', 'https://link.costam').tap do |event|
+        event.ball_possession = { home: 45, away: 55 }
+        event.attacks = { home: 1, away: 0 }
+        event.shots_off_target = { home: 3, away: 1 }
+        event.corners = { home: 1, away: 0 }
+      end
+    end
+
+    before do
+      stub_const('EventConditionChecker::MODEL_VALUE_CUTOFF', 1.45)
+      stub_const('EventConditionChecker::BALL_POSSESSION_ADVANTAGE_PERCENTAGE', 50)
+      stub_const('EventConditionChecker::BALL_POSSESSION_ADVANTAGE_PERCENTAGE_CUTOFF', 60)
+    end
+
+    context 'when model score is higher than cutoff value' do
+      before do
+        allow(described_class).to receive(:event_model_value).and_return(1.46)
+      end
+
+      context 'when possession is above cutoff value' do
+        before do
+          base_event.ball_possession = { home: 61, away: 39 }
+        end
+
+        it 'returns true' do
+          expect(described_class.should_be_reported?(base_event)).to be_truthy
+        end
+      end
+
+      context 'when possession is below cutoff value' do
+        it 'returns false' do
+          expect(described_class.should_be_reported?(base_event)).to be_falsey
+        end
+      end
+    end
+
+    context 'when model score is below the cutoff value' do
+      before do
+        allow(described_class).to receive(:event_model_value).and_return(1.36)
+      end
+
+      it 'returns false' do
+        expect(described_class.should_be_reported?(base_event)).to be_falsey
+      end
+    end
+  end
 end
