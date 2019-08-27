941fee3edcce1214fe9401d729574b4f17a79032
Marcin K <marcin.knychala@bitgamelabs.com>
2019-08-27 20:50:18 +0200

    Access configuration helper as a class

--- db/configuration_helper.rb
+++ db/configuration_helper.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class ConfigurationHelper
-  def establish_db_connection(env)
+  def self.establish_db_connection(env)
     ActiveRecord::Base.establish_connection(db_configuration[env])
   end
 
-  private
-
-  def db_configuration
+  def self.db_configuration
     db_configuration_file = File.join(File.expand_path(__dir__), '..', 'db', 'config.yml')
     YAML.safe_load(File.read(db_configuration_file), [], [], true)
   end
+
+  private_class_method :db_configuration
 end
