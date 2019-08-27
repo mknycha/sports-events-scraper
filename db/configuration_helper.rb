7cea047a399846dab2737995517f119fb79acecf
Marcin K <marcin.knychala@bitgamelabs.com>
2019-08-27 20:48:50 +0200

    Allow for aliases in db configuration helper

--- db/configuration_helper.rb
+++ db/configuration_helper.rb
@@ -9,6 +9,6 @@ class ConfigurationHelper
 
   def db_configuration
     db_configuration_file = File.join(File.expand_path(__dir__), '..', 'db', 'config.yml')
-    YAML.safe_load(File.read(db_configuration_file))
+    YAML.safe_load(File.read(db_configuration_file), [], [], true)
   end
 end
