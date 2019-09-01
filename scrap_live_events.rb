f633755ecd5bb3d8d3a8097bae31f62707c2777d
Marcin K <marcin.knychala@bitgamelabs.com>
2019-09-01 18:55:25 +0200

    Keep database initialization out of app itself

--- scrap_live_events.rb
+++ scrap_live_events.rb
@@ -2,12 +2,13 @@ Bundler.require
 require_relative 'load_files'
 require_relative 'mailer_initializer'
 
+db_configuration = Database::ConfigurationHelper.db_configuration[ENV['RUBY_ENV']]
+ActiveRecord::Base.establish_connection(db_configuration)
+
 class App
   def initialize
     @retries = 0
     @logger = DoubleLogger.new
-    db_configuration = Database::ConfigurationHelper.db_configuration[ENV['RUBY_ENV']]
-    ActiveRecord::Base.establish_connection(db_configuration)
   end
 
   def run
