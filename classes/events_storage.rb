1916d39c43ec2c20aa5898aecbda1e8f40ddaa3a
Marcin K <marcin.knychala@bitgamelabs.com>
2019-09-14 11:49:39 +0200

    Validate event before update, not only before creation
    To avoid bugs where 'Live' is passed to an event as a score

--- classes/events_storage.rb
+++ classes/events_storage.rb
@@ -7,17 +7,19 @@ class EventsStorage
   end
 
   def save_or_update_event(event_id, (name, time, score, link))
-    if event_exists?(event_id)
-      event = find_event(event_id)
-      event.update_time_and_score(time, score)
-      @logger.info "Temp storage: updated event\n#{event}"
-    else
+    event = find_event(event_id).dup
+    label = ''
+    if event.nil?
       event = Event.new(name, time, score, link)
-      if event.valid?
-        save_event(event_id, event)
-        @logger.info "Temp storage: added event\n#{event}"
-      end
+      label = 'added'
+    else
+      event.update_time_and_score(time, score)
+      label = 'updated'
     end
+    return event unless event.valid?
+
+    save_event(event_id, event)
+    @logger.info "Temp storage: #{label} event\n#{event}"
     event
   end
 
@@ -26,10 +28,6 @@ class EventsStorage
   end
 
   private
-
-  def event_exists?(event_id)
-    @events_hash.key?(event_id)
-  end
 
   def save_event(event_id, event)
     @events_hash[event_id] = event
